9488924af270813e6743bd1ce9fabeb5d3c2c2c4
David Hughes <hughesd9@tcd.ie>
2020-08-08 20:59:57 +0100

    Allowed the javascript file to find the grids, score and button in the index and also created the tetrominoes

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartButton = document.querySelector('#start-button')
+  const width = 10
+  //console.log(squares)
 
+  //Creating the Tetris shapes (Tetrominoes)
+  //Shapes can be rotated around 4 times
+  //Need to create array with the 4 possible positions that each shape can be in
+  const lTetrmino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
 
+  const zTetrmino = [
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
+  ]
 
+  const squareTetrmino = [
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
+  ]
+
+  const tTetrmino = [
+    [1, width, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
+  ]
+
+  const lineTetrmino = [
+    [1, width, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width3]
+  ]
+
+  const tetrominoes = [lTetrmino, zTetrmino, squareTetrmino, tTetrmino, lineTetrmino]
 })
